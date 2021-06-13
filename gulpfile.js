ba0297d470f6849198feeea6e62e6daf1d63b1c4
n-souta <vape1147@gmail.com>
2021-06-13 22:28:47 +0900

    初期設定

---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+const { src, dest, watch, series, parallel } = require( "gulp" );
+const sass = require( "gulp-dart-sass" );
+const plumber = require( "gulp-plumber" );
+const notify = require( "gulp-notify" );
+const postcss = require( "gulp-postcss" );
+const cssnext = require( "postcss-cssnext")
+const cleanCSS = require( "gulp-clean-css" );
+const rename = require( "gulp-rename" );
+const sourcemaps = require( "gulp-sourcemaps" );
+const babel = require( "gulp-babel" );
+const uglify = require( "gulp-uglify" );
+const imagemin = require( "gulp-imagemin" );
+const imageminMozjpeg = require( "imagemin-mozjpeg" );
+const imageminPngquant = require( "imagemin-pngquant" );
+const imageminSvgo = require( "imagemin-svgo" );
+const browserSync = require( "browser-sync" );
+
+
+const srcPath = {
+   css: 'src/css/**.scss',
+   js: 'src/js/**/*.js',
+   img: 'src/img/**/*',
+   html: '*.html',
+   php: '*.php',
+}
+const destPath = {
+   css: 'dist/css/',
+   js: 'dist/js/',
+   img: 'dist/img/',
+   html: 'dist/',
+   php: 'dist/',
+}
+const browsers = [
+   'last 2 versions',
+   '> 5%',
+   'ie = 11',
+   'not ie <= 10',
+   'ios >= 8',
+   'and_chr >= 5',
+   'Android >= 5',
+]
+const browserSyncOption = {
+  server: "./dist/"
+}
+
+const html = () => {
+    return src(srcPath.html)
+      .pipe(dest(destPath.html))
+  }
+const php = () => {
+    return src(srcPath.php)
+      .pipe(dest(destPath.php))
+  }
+
+const cssSass = () => {
+   return src( srcPath.css )
+       .pipe( sourcemaps.init() )
+       .pipe(
+           plumber(
+               {
+                   errorHandler: notify.onError( 'Error: <%= error.message %>' )
+               }
+           )
+       )
+       .pipe( sass() )
+       .pipe( postcss( [cssnext(browsers)] ) )
+       .pipe( sourcemaps.write( '/maps' ) )
+       .pipe( dest( destPath.css ) )
+       .pipe( cleanCSS() )
+       .pipe(
+           rename(
+               { extname: '.min.css' }
+           )
+       )
+       .pipe( sourcemaps.write( '/maps' ) )
+       .pipe( dest( destPath.css ) )
+}
+const jsBabel = () => {
+   return src( srcPath.js )
+       .pipe(
+           plumber(
+               {
+                   errorHandler: notify.onError( 'Error: <%= error.message %>' )
+               }
+           )
+       )
+       .pipe( babel( {
+           presets: [ '@babel/preset-env' ]
+       } ) )
+       .pipe( dest( destPath.js ) )
+       .pipe( uglify() )
+       .pipe(
+           rename(
+               { extname: '.min.js' }
+           )
+       )
+       .pipe( dest( destPath.js ) )
+}
+const imgImagemin = () => {
+   return src( srcPath.img )
+       .pipe(
+           imagemin(
+               [
+                   imageminMozjpeg({
+                       quality: 80
+                   }),
+                   imageminPngquant(),
+                   imageminSvgo({
+                       plugins: [
+                           {
+                               removeViewbox: false
+                           }
+                       ]
+                   })
+               ],
+               {
+                   verbose: true
+               }
+           )
+       )
+       .pipe( dest( destPath.img ) )
+}
+const browserSyncFunc = () => {
+   browserSync.init( browserSyncOption );
+}
+
+const browserSyncReload = ( done ) => {
+   browserSync.reload();
+   done();
+}
+const watchFiles = () => {
+   watch( srcPath.css, series( cssSass, browserSyncReload ) )
+   watch( srcPath.js, series( jsBabel, browserSyncReload ) )
+   watch( srcPath.img, series( imgImagemin, browserSyncReload ) )
+   watch( srcPath.html, series(html, browserSyncReload ) )
+   watch( srcPath.php, series(php, browserSyncReload ) )
+}
+exports.default = series( series( html,php,cssSass, jsBabel, imgImagemin ), parallel( watchFiles, browserSyncFunc ) );
+exports.build = series( html,php,cssSass, jsBabel, imgImagemin );
